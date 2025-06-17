188a4b9d27b01e467eda3d62106a4238af7b4f5c
Alexandre Pinheiro <alexclemente1985@gmail.com>
2025-06-17 17:25:59 -0300

    Projeto Imobiliária - atualização proprietário (carregamento dos dados)

--- projetoImob/src/views/painel/proprietario/index.php
+++ projetoImob/src/views/painel/proprietario/index.php
@@ -9,14 +9,14 @@
         ?>
             <h2 class="fonte26"><i class="fa-solid fa-user-tie fonte30 mg-r-1"></i>Cadastrar Proprietário</h2>
         <?php endif;?>
-        
+
     </div>
     <div class="limpar"></div>
     <form action="" method="POST" class="box-12 mg-t-8">
         <div class="box-12 mg-t-2">
             <!--Envio do id para evitar problema de valor nulo na hora do cadastro (mesmo sendo autoincremento)-->
             <input type="hidden"
-             value=<?php if(isset($id) && $id != ''): echo $proprietarios[0]->ID; endif;?> 
+             value=<?php if(isset($id) && $id != ''): echo $proprietarios[0]->ID; endif;?>
              name="id">
         </div>
         <div class="box-8">
@@ -31,16 +31,16 @@
             <label for="" class="fnc-preto-azulado">Contato</label>
             <input type="text"
              name="contato"
-             id=""
-             value=<?php if(isset($id) && $id != ''): echo $proprietarios[0]->CONTATO; endif;?>
+             id="contato"
+             value="<?php if(isset($id) && $id != ''): echo $proprietarios[0]->CONTATO; endif;?>"
               >
         </div>
         <div class="box-4">
             <label for="" class="fnc-preto-azulado">Sexo</label>
             <select name="sexo" id="sexo">
                 <option value="">Escolha o sexo...</option>
-                <option value="M" selected=<?=(isset($id) && $id != '' && $proprietarios[0]->SEXO == 'M')?>>Masculino</option>
-                <option value="F" selected=<?=(isset($id) && $id != '' && $proprietarios[0]->SEXO == 'F')?>>Feminino</option>
+                <option value="M" <?php if(isset($id) && $id != '' && $proprietarios[0]->SEXO == 'M'):?> selected="selected"<?php endif;?>>Masculino</option>
+                <option value="F" <?php if(isset($id) && $id != '' && $proprietarios[0]->SEXO == 'F'):?> selected="selected"<?php endif;?>>Feminino</option>
             </select>
         </div>
         <div class="box-12 mg-t-2">
