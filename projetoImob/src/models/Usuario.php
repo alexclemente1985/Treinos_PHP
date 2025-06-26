be8efbe6f6df49352b5a51303970786f53752560
Alexandre Pinheiro <alexclemente1985@gmail.com>
2025-06-26 15:14:15 -0300

    Projeto Imobiliária - Criação de Model Usuario

---
+++ projetoImob/src/models/Usuario.php
@@ -0,0 +1,66 @@
+<?php
+    class Usuario{
+        private string $id;
+        private string $nome;
+        private string $usuario;
+        private string $senha;
+        private string $email;
+        private string $imagem;
+        private string $datacadastro;
+        private string $ativo;
+
+        public function __construct(
+                string $id='',
+                string $nome='',
+                string $usuario='',
+                string $senha='',
+                string $email='',
+                string $imagem='',
+                string $datacadastro='',
+                string $ativo=''
+                )
+            {
+                $this->id = $id;
+                $this->nome = $nome;
+                $this->usuario = $usuario;
+                $this->senha = $senha;
+                $this->email = $email;
+                $this->imagem = $imagem;
+                $this->datacadastro = $datacadastro;
+                $this->ativo = $ativo;
+            }
+
+        public function __set($name, $value)
+        {
+            if(property_exists($this, $name)){
+                $this->$name = $value;
+            }
+        }
+
+        public function __get($name)
+        {
+            if (property_exists($this, $name)) {
+                return $this->$name;
+            }
+        }
+
+         public function getId(): int{
+            return $this->id;
+        }
+
+
+        public function toArray(){
+            return[
+                'id'=>$this->id,
+                'nome'=>$this->nome,
+                'usuario'=>$this->usuario,
+                'senha'=>$this->senha,
+                'email'=>$this->email,
+                'imagem'=>$this->imagem,
+                'datacadastro'=>$this->datacadastro,
+                'ativo'=>$this->ativo
+            ];
+        }
+
+    }
+?>
